f74dfa03088fddc2c4cb81304917cec3b9da5756
Sam Hofius <dxb@keybase.io>
2018-11-08 17:04:57 -0500

    Added SendTeamAPI()

---
+++ api/team_api.go
@@ -0,0 +1,51 @@
+package api
+
+import (
+	"encoding/json"
+	"log"
+	"os/exec"
+)
+
+type TeamAPIResponse struct {
+	Result teamAPIResult `json:"result,omitempty"`
+	Error  teamAPIError  `json:"error,omitempty"`
+}
+
+type teamAPIResult struct {
+	ChatSent     bool `json:"chatSent"`
+	CreatorAdded bool `json:"creatorAdded"`
+}
+
+type teamAPIError struct {
+	Code    int    `json:"code"`
+	Message String `json:"message"`
+}
+
+func SendTeamAPI(jsonData string) TeamAPIResponse {
+	log.Println("[SendTeamAPI]", "[out]", jsonData)
+	cmd := exec.Command("keybase", "team", "api", "-m", jsonData)
+
+	cmdOut, err := cmd.Output()
+	if err != nil {
+		panic(err)
+	}
+
+	var retVal TeamAPIResponse
+	json.Unmarshal(cmdOut, &retVal)
+
+	if retVal.Error != nil {
+		log.Printf(
+			"[SendTeamAPI] [in] [Error] [Code: %d] [Message: %s]\n",
+			retVal.Error.Code,
+			retVal.Error.Message,
+		)
+	} else {
+		log.Printf(
+			"[SendTeamAPI] [in] [ChatSent: %v] [CreatorAdded: %v]\n",
+			retVal.Result.ChatSent,
+			retVal.Result.CreatorAdded,
+		)
+	}
+
+	return retVal
+}
