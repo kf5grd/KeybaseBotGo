93bbde5c936a401ee854eda8ae2605f24c91ce1b
Sam Hofius <dxb@keybase.io>
2018-11-08 17:19:17 -0500

    Added error handling

--- api/chat_api.go
+++ api/chat_api.go
@@ -7,19 +7,25 @@ import (
 )
 
 type ChatAPIResponse struct {
-	Result ChatAPIResult `json:"result"`
+	Result chatAPIResult `json:"result,omitempty"`
+	Error  chatAPIError  `json:"error,omitempty"`
 }
 
-type ChatAPIResult struct {
+type chatAPIResult struct {
 	Message    string                `json:"message"`
-	RateLimits []ChatAPIResultLimits `json:"ratelimits"`
+	RateLimits []chatAPIResultLimits `json:"ratelimits"`
 }
 
-type ChatAPIResultLimits struct {
+type chatAPIResultLimits struct {
 	Tank     string `json:"tank"`
 	Capacity int    `json:"capacity"`
 	Reset    int    `json:"reset"`
 	Gas      int    `json:"gas"`
+}
+
+type chatAPIError struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
 }
 
 func SendChatAPI(jsonData string) ChatAPIResponse {
@@ -34,6 +40,21 @@ func SendChatAPI(jsonData string) ChatAPIResponse {
 	var retVal ChatAPIResponse
 	json.Unmarshal(cmdOut, &retVal)
 
-	log.Printf("[SendChatAPI] [in] [Tank: %s] [Capacity: %d] [Reset: %d] [Gas: %d]", retVal.Result.RateLimits[0].Tank, retVal.Result.RateLimits[0].Capacity, retVal.Result.RateLimits[0].Reset, retVal.Result.RateLimits[0].Gas)
+	if retVal.Error.Message != "" {
+		log.Printf(
+			"[SendChatAPI] [in] [Error] [Code: %d] [Message: %s]\n",
+			retVal.Error.Code,
+			retVal.Error.Message,
+		)
+	} else {
+		log.Printf(
+			"[SendChatAPI] [in] [Tank: %s] [Capacity: %d] [Reset: %d] [Gas: %d]",
+			retVal.Result.RateLimits[0].Tank,
+			retVal.Result.RateLimits[0].Capacity,
+			retVal.Result.RateLimits[0].Reset,
+			retVal.Result.RateLimits[0].Gas,
+		)
+	}
+
 	return retVal
 }
